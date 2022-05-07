b3f013632b6b8d0050940ce832203c50fb02dfb7
Picouet <vincent.picouet@gmail.com>
2022-05-07 17:04:23 -0400

    Deployed b3497d1 with MkDocs version: 1.3.0

--- merge_temp_files.py
+++ merge_temp_files.py
@@ -2,6 +2,8 @@ from astropy.table import Table, vstack, hstack
 import os
 import glob
 import subprocess
+import numpy as np
+from astropy.io import ascii 
 subprocess.call(["ls", "-l"])
 
 
@@ -19,28 +21,35 @@ for folder in folders[:]:
     print(folder)
     if os.path.isfile(folder + '/alltemps.csv'):
         temp = Table.read(folder + '/alltemps.csv')[::n]
+        for col in temp.colnames[1:]:
+            temp[col][0]=np.nan 
+            temp[col][-1]=np.nan 
         tables.append(temp)
+
 
     if os.path.isfile(folder + '/pressure.csv'):
         pressure = Table.read(folder + '/pressure.csv')[::n]
-        print(pressure[0])
+        for col in pressure.colnames[1:]:
+            pressure[col][0] = np.nan
+            pressure[col][-1] = np.nan
         tables_press.append(pressure)
-    
+
 # tables_styack = vstack(tables,join_type='inner')
 tables_styack = vstack(tables,join_type='outer')
 tables_press_styack = vstack(tables_press)
 # for col in tables_styack.colnames:
 #     try:
-#         tables_styack[col][(tables_styack[col]== np.ma.core.MaskedConstant).mask]=0#np.inf 
+#         tables_styack[col][(tables_styack[col]== np.ma.core.MaskedConstant).mask]=0#np.inf
 #     except AttributeError:
 #         pass
 temp_path = '/Users/Vincent/Nextcloud/LAM/FIREBALL/2022/temperatures/all/alltemps.csv'
 press_path = '/Users/Vincent/Nextcloud/LAM/FIREBALL/2022/temperatures/all/pressure.csv'
-tables_styack.write(temp_path,overwrite=True)
-tables_press_styack.write(press_path,overwrite=True)
-    
+tables_styack.write(temp_path,overwrite=True,fill_values=[(ascii.masked, "NaN")])
+tables_press_styack.write(press_path, overwrite=True, fill_values=[(np.nan, "null")])
+
 import shutil
 shutil.copy(press_path, "/Users/Vincent/Github/pyds9plugin-doc/docs/temperatures/pressure.csv")
 shutil.copy(temp_path, "/Users/Vincent/Github/pyds9plugin-doc/docs/temperatures/alltemps.csv")
-    
-    
+
+
+#%%
